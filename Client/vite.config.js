99e4df05f07460cd652f0fbca5501eb7f7fd65b0
dvimukthi <deshanvimukthi59@gmail.com>
2025-05-04 22:14:32 +0530

    base edit

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
-import process from "node:process";
+import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-  // Load .env variables
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react(), tailwindcss()],
-    base: env.VITE_BASE_PATH || "/events-website",
-  };
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "/events-website",
 });
