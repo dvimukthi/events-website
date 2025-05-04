918f1300b8748ddfec9a2097cdb2346585565819
dvimukthi <deshanvimukthi59@gmail.com>
2025-05-04 21:50:54 +0530

    fix the error

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from "vite";
+import process from "node:process";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || "/events-website",
+export default defineConfig(({ mode }) => {
+  // Load .env variables
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base: env.VITE_BASE_PATH || "/events-website",
+  };
 });
